57022280a3f371fdaf1b04e5a11ad480a1851483
THARUNJP <tharunroshini71@gmail.com>
2026-04-18 17:37:42 +0530

    toast added

---
+++ src/lib/toast.ts
@@ -0,0 +1,21 @@
+import toast from "react-hot-toast";
+
+
+export const showHotToast = (
+  message: string,
+  type: "success" | "error" | "loading" | "custom" = "success"
+) => {
+  switch (type) {
+    case "success":
+      toast.success(message);
+      break;
+    case "error":
+      toast.error(message);
+      break;
+    case "loading":
+      toast.loading(message);
+      break;
+    default:
+      toast(message);
+  }
+};
